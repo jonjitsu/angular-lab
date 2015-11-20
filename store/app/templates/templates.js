2c30e11edd1dc9a0517d58cfbe710c41bb7991c4
Jonathan Willis <jonathanwillis@linux.com>
2015-11-20 09:12:18 -0500

    Added Factory. Use factory in templates.

--- store/app/templates/templates.js
+++ store/app/templates/templates.js
@@ -1,7 +1,10 @@
 angular
     .module('store.templates', ['ngRoute'])
     .config(['$routeProvider', TemplatesRouting])
-    .controller('TemplatesCtrl', ['$scope', TemplatesController])
+    .constant('TEMPLATES_API_URL', '/app/data/templates.json')
+    .factory('TemplatesFactory', ['$http', 'TEMPLATES_API_URL', TemplatesFactory])
+    .controller('TemplatesCtrl', ['$scope', 'TemplatesFactory', TemplatesCtrl])
+    .controller('TemplateDetailsCtrl', ['$scope', '$routeParams', 'TemplatesFactory', TemplateDetailsCtrl])
 ;
 
 function TemplatesRouting($routeProvider) {
@@ -9,9 +12,56 @@ function TemplatesRouting($routeProvider) {
         .when('/templates', {
             templateUrl: 'templates/templates.html',
             controller: 'TemplatesCtrl'
-        });
+        })
+        .when('/templates/:templateId', {
+            templateUrl: 'templates/template-details.html',
+            controller: 'TemplateDetailsCtrl'
+        })
 }
 
-function TemplatesController($scope) {
-    console.log($scope);
+function update(scope, prop) {
+    return function(data) {
+        console.log(data);
+        scope[prop]=data;
+    };
+}
+
+function TemplatesCtrl($scope, TemplatesFactory) {
+    var vm = $scope;
+
+    vm.templates = [];
+    TemplatesFactory
+        .all()
+        .then(update(vm, 'templates'));
+}
+
+function TemplateDetailsCtrl($scope, $routeParams, TemplatesFactory) {
+    var vm = $scope; 
+
+    vm.template = {};
+    TemplatesFactory
+        .get(parseInt($routeParams.templateId, 10))
+        .then(update(vm, 'template'));
+}
+
+function TemplatesFactory($http, API_URL) {
+    return {
+        all: function() {
+            return $http
+                .get(API_URL)
+                .then(function(response) {
+                    return response.data;
+                });
+        },
+        get: function(id) {
+            return this.all()
+                .then(function(templates) {
+                    return Lazy(templates)
+                        .filter(function(template) {
+                            return template.id === id;
+                        })
+                        .first();
+                })
+        }
+    };
 }
