1c60117fd57a8432576a6702f19029248c403ebc
Jonathan Willis <jonathanwillis@linux.com>
2015-11-25 10:45:07 -0500

    Added mocha test framework and gulp task

--- addressbook/gulpfile.js
+++ addressbook/gulpfile.js
@@ -17,3 +17,19 @@ gulp.task('serve', function() {
     gulp.watch(['app/**/*.*'])
         .on('change', bs.reload);
 });
+
+gulp.task('serve-test', function() {
+    bs.init({
+        notify: false,
+        port: 8081,
+        server: {
+            baseDir:["test", "app"],
+            routes: {
+                '/bower_components':'bower_components'
+            }
+        }
+    });
+
+    gulp.watch(['app/**/*.*', 'test/**/*.js'])
+        .on('change', bs.reload);
+});
---
+++ addressbook/test/main.spec.js
@@ -0,0 +1,6 @@
+
+describe("The Address Book App", function(){
+    it("should work", function() {
+        chai.assert.isArray([]);
+    });
+});
